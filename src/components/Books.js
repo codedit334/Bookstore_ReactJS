3435b461528a9e93b7a15e7f393560c7a61fa131
codedit334 <oussama7elabdioui@gmail.com>
2023-03-29 17:47:24 +0000

    initialize project with components

--- src/components/Books.js
+++ src/components/Books.js
@@ -36,6 +36,14 @@ export default function Books() {
           </li>
         ))}
       </ul>
+      <div className='book-form'>
+          <h2>Add Book</h2>
+          <form>
+              <input placeholder='Book title' />
+              <input placeholder='Book author' />
+              <input type={"submit"} value="Submit" />
+          </form>
+      </div>
     </div>
   );
 }
